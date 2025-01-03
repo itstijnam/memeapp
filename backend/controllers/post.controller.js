206fd83c26c7d09bc6325c1cb33dcae101b4c48e
itstijnam <manjitsyngh@gmail.com>
2025-01-03 09:25:00 +0530

    postControl

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -4,6 +4,7 @@ import path from 'path';
 import fs from 'fs';
 import { Post } from '../models/post.model.js';
 import { User } from '../models/User.model.js';
+import {Comment} from '../models/comment.model.js';
 
 export const addNewPost = async (req, res) => {
     try {
@@ -67,9 +68,181 @@ export const addNewPost = async (req, res) => {
 };
 
 
-export const getAllPost = (req, res)=>{
+export const getAllPost = async (req, res)=>{
     try {
+        const post = await Post.find().sort({createdAt: -1})
+        .populate({path:'author', select:'username, profilePicture'})
+        .populate({
+            path: 'comments',
+            sort:{createdAt: -1},
+            populate:{
+                path: 'author',
+                select: 'username profilePicture'
+            }
+        });
+        return res.status(200).json({
+            post,
+            success:true 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const getUserPost = async (req, res)=>{
+    try {
+        const authorId = req.id;
+        const posts = await Post.find({author: authorId}).sort({createdAt: -1})
+        .populate({path:'author', select:'username, profilePicture'})
+        .populate({
+            path: 'comments',
+            sort:{createdAt: -1},
+            populate:{
+                path: 'author',
+                select: 'username profilePicture'
+            }
+        });
+        return res.status(200).json({
+            posts,
+            success:true 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const likePost = async (req,res)=>{
+    try {
+        const likeKarneWaaleUserkiId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message: 'Post not found', success:false});
+
+        // like logic started
+        await post.updateOne({$addToSet:{likes: likeKarneWaaleUserkiId}})
+        await post.save()
+
+        // socket.io will be here for notification
+
+        return res.status(200).json({
+            message: 'Post Liked',
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const dislikePost = async (req,res)=>{
+    try {
+        const likeKarneWaaleUserkiId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message: 'Post not found', success:false});
+
+        // dislike logic started
+        await post.updateOne({$pull:{likes: likeKarneWaaleUserkiId}})
+        await post.save()
+
+        // socket.io will be here for notification
+
+        return res.status(200).json({
+            message: 'Post disliked',
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const addComment = async (req,res)=>{
+    try {
+        const postId = req.params.id;
+        const commentWaaleKiId = req.id;
+
+        const {text} = req.body;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message:'Something went wrong', success:false})
+        if(!text) return res.status(400).json({message:'message required', success:false})
         
+        const comment = await Comment.create({
+            text,
+            author:commentWaaleKiId,
+            post:postId
+        }).populate({
+            path: 'author',
+            select: 'username profilePicture'
+        });
+
+        post.comments.push(comment._id);
+        await post.save();
+
+        return res.status(201).json({
+            message: 'Comment Added',
+            comment,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+export const getCommentsOfPost = async (req, res)=>{
+    try {
+        const postId = req.params.id;
+        const comments = await Comment.find({post:postId}).populate('author', 'username profilePicture');
+        if(!comments) return res.status(404).json({message: 'No comments are found', success: false});
+        return res.status(200).json({success: true, comments});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deletePost = async (req, res)=>{
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message: 'post not found', success: false});
+
+        // check if the logged in user is the owner of the post
+        if(post.author.toString() !== authorId) return res.status(403).json({message: 'unauthorized'})
+        // delete post
+        await Post.findByIdAndDelete(postId);
+
+        // remove the post id from the user's post
+        let user = await User.findById(authorId);
+        user.posts = user.posts.filter(id => id.toString() !== postId); 
+        await user.save();
+
+        await Comment.deleteMany({post:postId});
+
+        return res.status(200).json({
+            message: 'Post Deleted',
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const bookmarksPost =  async (req, res)=>{
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+        const post =  await Post.findById(postId);
+        if(!post) return res.status(404).json({message: 'post not found', success: false});
+
+        const user = await User.findById(authorId);
+        if(user.bookmarks.includes(post._id)){
+            await user.updateOne({$pull:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({type:'unsaved', message:'Post removed from bookmark', success: true});
+        }else{
+            await user.updateOne({$addToSet:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({type:'saved', message:'Post bookmarked', success: true});
+        }
     } catch (error) {
         console.log(error)
     }
